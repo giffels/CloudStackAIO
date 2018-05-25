a0eaf69ce992047680b1040ea0956a1b2ece991a
Manuel Giffels <giffels@gmail.com>
2018-05-25 14:13:17 +0200

    Added type hints

--- CloudStackAIO/CloudStack.py
+++ CloudStackAIO/CloudStack.py
@@ -1,6 +1,7 @@
 from functools import partial
 from urllib.parse import quote
 from urllib.parse import urlencode
+from typing import Callable
 
 import asyncio
 import aiohttp
@@ -15,7 +16,8 @@ class CloudStackClientException(Exception):
 
 
 class CloudStack(object):
-    def __init__(self, end_point, api_key, api_secret, event_loop, async_poll_latency=2):
+    def __init__(self, end_point: str, api_key: str, api_secret: str,
+                 event_loop: asyncio.AbstractEventLoop, async_poll_latency: int=2) -> None:
         self.end_point = end_point
         self.api_key = api_key
         self.api_secret = api_secret
@@ -23,26 +25,26 @@ class CloudStack(object):
         self.async_poll_latency = async_poll_latency
         self.client_session = aiohttp.ClientSession(loop=self.event_loop)
 
-    def __del__(self):
+    def __del__(self) -> None:
         try:
             self.event_loop.run_until_complete(self.close_session())
         except RuntimeError:
             self.event_loop.create_task(self.close_session())
 
-    async def close_session(self):
+    async def close_session(self) -> None:
         await self.client_session.close()
         await asyncio.sleep(0.25)  # http://aiohttp.readthedocs.io/en/stable/client_advanced.html#graceful-shutdown
 
-    def __getattr__(self, command):
+    def __getattr__(self, command: str) -> Callable:
         return partial(self.request, command=command)
 
-    async def request(self, command, **kwargs):
+    async def request(self, command: str, **kwargs) -> dict:
         kwargs.update(dict(apikey=self.api_key, command=command))
         async with self.client_session.get(self.end_point, params=self.sign(kwargs)) as response:
             return await self.handle_response(response=response,
                                               await_final_result='queryasyncjobresult' not in command.lower())
 
-    async def handle_response(self, response, await_final_result):
+    async def handle_response(self, response: aiohttp.client_reqrep.ClientResponse, await_final_result: bool) -> dict:
         try:
             data = await response.json()
         except aiohttp.client_exceptions.ContentTypeError:
@@ -56,7 +58,7 @@ class CloudStack(object):
             await asyncio.sleep(self.async_poll_latency)
             data = await self.queryAsyncJobResult(jobid=data['jobid'])
             if data['jobstatus']:  # jobstatus is 0 for pending async CloudStack calls
-                if not data['jobresultcode']:  # exit code not zero
+                if not data['jobresultcode']:  # exit code is zero
                     try:
                         return data['jobresults']
                     except KeyError:
@@ -66,7 +68,7 @@ class CloudStack(object):
 
         return data
 
-    def sign(self, url_parameters):
+    def sign(self, url_parameters: dict) -> dict:
         if url_parameters:
             url_parameters.pop('signature', None)  # remove potential existing signature from url parameters
             request_string = urlencode(sorted(url_parameters.items()), safe='.-*_', quote_via=quote).lower()
@@ -75,6 +77,6 @@ class CloudStack(object):
         return url_parameters
 
     @staticmethod
-    def transform_data(data):
+    def transform_data(data: dict) -> dict:
         for key in data.keys():
             return data[key]
